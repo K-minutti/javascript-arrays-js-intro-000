23acfdc9908a599a257199d0beaa109c0baa5c72
Kevin Minutti <minutti70@gmail.com>
2020-03-21 14:23:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,12 @@
 var chocolateBars = [
 "snickers", "hundred grand", "kitkat", "skittles"
 ]
+addElementToBeginningOfArray and destructivelyAddElementToBeginningOfArray.
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+
+function estructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
